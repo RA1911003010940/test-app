a054ef6a17b458410567f2deb8734eb5ce32773b
Rohith Kumar <rohithkumar6043@gmail.com>
2022-08-13 12:16:31 +0530

    added files commit 1

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express")
+require('dotenv').config()
+const app = express()
+
+const {MongoClient} = require('mongodb');
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+const dbName = 'books';
+const db = client.db(dbName);
+const collection = db.collection('books');
+
+app.use("/static",express.static("static"))
+
+app.get("/books", async function(req,res){
+    let list = await collection.find({}).toArray();
+
+    res.json(list)
+})
+
+app.listen(3000)
